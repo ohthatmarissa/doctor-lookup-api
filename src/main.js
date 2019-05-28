897d36df0cea81161639334dd3ba5ade70af80fb
Marissa Perry <ohthatmarissa@gmail.com>
2019-05-27 23:19:49 -0700

    add all the node shit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { pingPong } from './ping-pong';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
