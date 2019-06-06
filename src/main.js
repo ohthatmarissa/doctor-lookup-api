6f7f811ea2078ea810840201a92f5315cb482d82
Marissa Perry <ohthatmarissa@gmail.com>
2019-06-06 12:07:05 -0700

    add everything needed to run basic api calls

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
-import { pingPong } from './ping-pong';
-import './styles.css';
 import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
+    $('#weatherLocation').click(function() {
+      let city = $('#location').val();
+      $('#location').val("");
+      $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c9781f5f2386e09424b4ffd04b0deb8b`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+        },
+        error: function() {
+          $('#errors').text("There was an error processing your request. Please try again.");
+        }
+      });
     });
   });
-});
+// c9781f5f2386e09424b4ffd04b0deb8b
