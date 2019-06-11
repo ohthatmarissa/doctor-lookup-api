02b77a581bc14395a6c63b54e7f2e2b387ab2626
Marissa Perry <ohthatmarissa@gmail.com>
2019-06-11 14:43:45 -0700

    add funtional jquery fro return list of drs

--- src/doctors.js
+++ src/doctors.js
@@ -2,7 +2,7 @@ export class DoctorService {
     getDoctorByQuery(query) {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=wa-seattle&limit=10&user_key=f01cd61ef76432e58af10a28661bec99`;
+        const url = `https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=wa-seattle&limit=10&user_key=f01cd61ef76432e58af10a28661bec99`;
         request.onload = function() {
           if (this.status === 200) {
             resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,22 +1,34 @@
 import { DoctorService } from './doctors.js';
 import $ from 'jquery';
 
+
 $(document).ready(function() {
-  $('#doctorQuery').click(function() {
+  $('#doctorQuery').click(function(event) {
+    event.preventDefault();
     let query = $('#query').val();
     $('#query').val("");
     console.log(query);
 
 
-    let doctorService = new DoctorService();  // create instance of WeatherService class
-    let promise = doctorService.getDoctorByQuery(query);  // call the instance method and pass in user input
-
+    let newDoctorService = new DoctorService();  // create instance of DoctorService class
+    let promise = newDoctorService.getDoctorByQuery(query);  // call the instance method and pass in user input
     promise.then(function(response) {
       let body = JSON.parse(response);
       
-      $('.showDoctor').text(`The Doctors in ${query} are ${body.data.profile.first_name}%`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      if(body.data.length > 0) {
+        $('#searchResults').text(' ');
+      
+        for(let i=0; i<body.data.length; i++) {
+          if (body.data[i].practices[0].accepts_new_patients == true) {
+            $('#searchResults').append("<ul>" + "<li>" + "Doctor: " + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "<br>" + "Specialties: " + body.data[i].specialties[0].specialty_uid + "<br>" + "Phone Number: " + body.data[i].practices[0].phones[0].number + "<br>" + "Practice Website: " + body.data[i].practices[0].website + "<br>" + "Accepting New Patients" + "</li>" + "</ul>");
+          } else {
+            $('#searchResults').append("<ul>" + "<li>" + "Doctor: " + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "<br>" + "Specialties: " + body.data[i].specialties[0].specialty_uid + "<br>" + "Phone Number: " + body.data[i].practices[0].phones[0].number + "<br>" + "Practice Website: " + body.data[i].practices[0].website + "<br>" + "NOT Accepting New Patients" + "</li>" + "</ul>");
+          }
+        }
+      } else {
+        $('#searchResults').text('No Doctors Match Your Search');
+      }
+      return newDoctorService.getDoctorByQuery(query);
     });
   });
 
